4c59c1ee771a497ecaca664ac6532f5f416747b4
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-05-25 18:48:04 -0500

    Start the use of classes in Java

---
+++ Course/src/CountProve.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountProve {
+
+	public static void main (String [] args) {
+		
+		Scanner inn = new Scanner (System.in);
+		
+		Count myCount = new Count();
+		
+		System.out.printf ("The initial name is: %s%n%n", myCount.getName());
+		
+		System.out.println ("Introduce the name: ");
+		String theName = inn.nextLine(); // Type String (for letters)
+		myCount.setName(theName); //Introduce the name in «myCount»
+		System.out.println();
+		
+		System.out.printf ("The name in the object 'myCount' is: %n%s%n", myCount.getName());
+
+	}
+
+}
