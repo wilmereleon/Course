6022a7c7a8812c752c826300043c934b34e52f31
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-05-28 10:05:16 -0500

    Constructors

---
+++ Course/src/CountProve2.java
@@ -0,0 +1,14 @@
+
+public class CountProve2 {
+
+	public static void main (String [] args) {
+		
+		Count2 count1 = new Count2("Jane Green");
+		Count2 count2 = new Count2("Ana Franklin");
+		
+		System.out.printf ("The name in count1 is: %s%n", count1.getName());
+		System.out.printf ("The name in count2 is: %s%n", count2.getName());
+
+	}
+
+}
